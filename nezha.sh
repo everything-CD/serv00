46bd1cb5d8c8951e89fe239cf58ce478f3f4f5f0
everything-CD <yingshan980808@gmail.com>
2024-08-15 20:48:55 +0800

    Update nezha.sh

--- nezha.sh
+++ nezha.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+####### nezha #################
 USER=$(whoami)
 WORKDIR="/home/${USER,,}/.nezha-dashboard"
 SCRIPT="${WORKDIR}/start.sh"
 CRON_SCRIPT="nohup ${SCRIPT} >/dev/null 2>&1 &"
-
+#### nezha-agent #######################
+WORKDIR2="/home/${USER,,}/.nezha-agent"
+CRON_NEZHA="nohup ${WORKDIR2}/start.sh >/dev/null 2>&1 &"
+################################################
 echo "检查并添加 crontab 任务"
 
+# 确认主脚本是否存在
 if [ -e "${SCRIPT}" ]; then
-    echo "添加 nezha 的 crontab 重启任务"
+    echo "添加 nezha 和 nezha-agent 的 crontab 重启任务"
 
-    (crontab -l | grep -F "@reboot pkill -f ${SCRIPT} || ${CRON_SCRIPT}") || \
-    (crontab -l; echo "@reboot pkill -f ${SCRIPT} || ${CRON_SCRIPT}") | crontab -
+    # 添加 Nezha Dashboard 的重启任务
+    if ! crontab -l | grep -qF "@reboot pkill -f ${SCRIPT} || ${CRON_SCRIPT}"; then
+        (crontab -l; echo "@reboot pkill -f ${SCRIPT} || ${CRON_SCRIPT}") | crontab -
+    fi
 
-    (crontab -l | grep -F "* * pgrep -f ${SCRIPT} > /dev/null || ${CRON_SCRIPT}") || \
-    (crontab -l; echo "0 * * * * pgrep -f ${SCRIPT} > /dev/null || ${CRON_SCRIPT}") | crontab -
+    # 添加 Nezha Agent 的重启任务
+    if ! crontab -l | grep -qF "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}"; then
+        (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") | crontab -
+    fi
+
+    # 每12分钟检查 Nezha Agent 是否运行
+    if ! crontab -l | grep -qF "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}"; then
+        (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
+    fi
+
+    # 每小时检查 Nezha Dashboard 是否运行
+    if ! crontab -l | grep -qF "0 * * * * pgrep -f ${SCRIPT} > /dev/null || ${CRON_SCRIPT}"; then
+        (crontab -l; echo "0 * * * * pgrep -f ${SCRIPT} > /dev/null || ${CRON_SCRIPT}") | crontab -
+    fi
+
 else
     echo "脚本 ${SCRIPT} 不存在，无法设置 crontab 任务"
 fi
